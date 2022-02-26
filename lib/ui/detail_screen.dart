9c4c088e62e506c6fcd9a44cda3b314cc941cc55
Shravanti Sunchu <shravanti.sunchu@kotak.com>
2022-02-26 13:36:26 +0530

    Added Back Arrow on the Detail Screen

--- lib/ui/detail_screen.dart
+++ lib/ui/detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:teji_mandi/constants.dart/constant_data.dart';
 import 'package:teji_mandi/dynamic_widgets/title_text.dart';
+import 'package:teji_mandi/utils/colors.dart';
 
 // ignore: must_be_immutable
 class DetailScreen extends StatelessWidget {
@@ -42,21 +43,44 @@ class DetailScreen extends StatelessWidget {
                 ),
               ),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  TitleText(
-                    title: title,
-                    screenName: ConstantsData().detailScreen,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:  [
+                      InkWell(
+                        onTap: () => Navigator.pop(context),
+                        child: Padding(
+                          padding: const EdgeInsets.only(top:42,left:24),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.black.withOpacity(0.45),
+                            radius: 25,
+                            child:const Icon(Icons.arrow_back, color: ColorUtils.appBarTitleColor,),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  SubTitleText(
-                    title: subTitle,
-                    date: date,
-                    screenName: ConstantsData().detailScreen,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      TitleText(
+                        title: title,
+                        screenName: ConstantsData().detailScreen,
+                      ),
+                      SubTitleText(
+                        title: subTitle,
+                        date: date,
+                        screenName: ConstantsData().detailScreen,
+                      ),
+                      DescriptionText(
+                        title: description,
+                      )
+                    ],
                   ),
-                  DescriptionText(
-                    title: description,
-                  )
                 ],
               ),
             )),
